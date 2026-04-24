a957f90e977fb7a1697dfa929726b68f087341de
Nichole Cruzalegui <n.cruzalegui@hotmail.com>
2026-04-24 16:07:39 -0400

    Updates

---
+++ precache-manifest.b63d8942caf86ea361a989f42b139491.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02bed1764082ab39eaaf2264616e8c57",
+    "url": "/haicoo-demo/index.html"
+  },
+  {
+    "revision": "9cf659acd0f713175fed",
+    "url": "/haicoo-demo/static/css/2.20e033e9.chunk.css"
+  },
+  {
+    "revision": "6f1c42cd392a0e67c26f",
+    "url": "/haicoo-demo/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "9cf659acd0f713175fed",
+    "url": "/haicoo-demo/static/js/2.7efbe9ae.chunk.js"
+  },
+  {
+    "revision": "921adb173c5d83dcf71d6a9bc3cbe81a",
+    "url": "/haicoo-demo/static/js/2.7efbe9ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f1c42cd392a0e67c26f",
+    "url": "/haicoo-demo/static/js/main.9264bbe4.chunk.js"
+  },
+  {
+    "revision": "60ec937e31879025898b",
+    "url": "/haicoo-demo/static/js/runtime-main.a1729077.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/haicoo-demo/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/haicoo-demo/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/haicoo-demo/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/haicoo-demo/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/haicoo-demo/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
